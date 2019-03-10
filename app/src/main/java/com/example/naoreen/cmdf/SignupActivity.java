a237036782535d11162dbd47d76ea9f6f552fff5
Naoreen <o7r1b@cs.ubc.ca>
2019-03-09 18:57:44 -0800

    updated manifest to open on login activity

--- app/src/main/java/com/example/naoreen/cmdf/SignupActivity.java
+++ app/src/main/java/com/example/naoreen/cmdf/SignupActivity.java
@@ -2,9 +2,16 @@ package com.example.naoreen.cmdf;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SignupActivity extends AppCompatActivity {
     private static final String TAG = "SignupActivity";
+
+    @BindView(R.id.link_login) TextView _loginLink;
 
 
 
@@ -12,7 +19,18 @@ public class SignupActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                finish();
+            }
+        });
     }
+
+
 
 
 
