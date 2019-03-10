a9c07d0a3f9d7c60866ecabed1a55e73f9009c63
Naoreen <o7r1b@cs.ubc.ca>
2019-03-10 12:04:35 -0700

    force push - fixed Alice Bob bug

--- app/src/main/java/com/example/naoreen/cmdf/NewTask.java
+++ app/src/main/java/com/example/naoreen/cmdf/NewTask.java
@@ -10,14 +10,30 @@ import android.widget.Spinner;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class NewTask extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    public ArrayList<JSONObject> allTasks;
+    public ArrayList<String> members;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+        members = new ArrayList<>();
+        members.add("Alice");
+        members.add("Bob");
+
 
         //TODO: display members -- Parse JSON file to get list of users
+        Spinner usr = findViewById(R.id.member);
+        ArrayAdapter<String> adapter0 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, members);
+        //parse JSON file
+        adapter0.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        usr.setAdapter(adapter0);
+        usr.setOnItemSelectedListener(this);
+
+
 
         // display task difficulty level
         Spinner pnt = findViewById(R.id.point);
@@ -25,10 +41,13 @@ public class NewTask extends AppCompatActivity implements AdapterView.OnItemSele
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         pnt.setAdapter(adapter);
         pnt.setOnItemSelectedListener(this);
+
+        allTasks = new ArrayList<>();
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //Toast.makeText(getApplicationContext(), parent.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
 
     }
 
@@ -52,10 +71,23 @@ public class NewTask extends AppCompatActivity implements AdapterView.OnItemSele
         EditText dateText = findViewById(R.id.date);
         String date = dateText.getText().toString();
 
-        // Status
-        String status = "TODO";
+        //Member
+        Spinner m = findViewById(R.id.member);
+        String mem = m.getSelectedItem().toString();
+
 
         JSONObject task = new JSONObject();
+        try{
+            task.put("description", des);
+            task.put("point", point);
+            task.put("dueDate", date);
+            task.put("status", "TODO");
+            task.put("member", mem);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        allTasks.add(task);
 
 
     }
